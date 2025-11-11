eb47e91cb42a1404b9c9d1abe42a7ee0494a3673
Iquenshu <ex20375@gmail.com>
2025-11-11 14:50:11 +0800

    Add files via upload
    修正檔案 不要keepalive

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'✅ Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+# 啟動
+token = os.getenv("TOKEN")
+if not token:
+    raise ValueError("❌ TOKEN not set in environment variables.")
+client.run(token)
